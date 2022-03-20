b532abf0e7bd360b9f7759a4e673466dba6198e6
maddnty <nzkb24@gmail.com>
2022-03-20 17:45:47 -0500

    stored character values in specified arrays

--- Develop/script.js
+++ Develop/script.js
@@ -19,12 +19,99 @@ function writePassword() {
 
 // }
 console.log("hi");
-const array = ["u", "l", "t", "i", "m", "a", "t", "e", "p", "a", "s", "s", "w"
-, "o", "r", "d"];
 passwordLength = 8;
 for (i = 0; i < passwordLength; i++){
-  console.log(array);
+  let sam = Math.floor(65);
+  console.log(sam);
 }
+
+// const lower = 97;
+// const lowerChar = [];
+
+// function mapLower() {
+  
+//   for(i = 97; i <= 122; i++){
+//     String.fromCharCode(i);
+//     lowerChar.push(i)
+//   }
+//   return lowerChar;
+// }
+
+let char = String.fromCharCode(100, 99, 111, 100, 101);
+
+console.log(char);
+
+// defines arrays to hold characters
+const lowerArray = [];
+const upperArray = [];
+const numberArray = [];
+const symbolArray = [];
+
+// defines
+lower = lowerCharacters(97, 122)
+upper = upperCharacters(65, 90)
+number = numbers(48, 57)
+symbol = symbols(33, 47).concat(
+  symbols(58, 64).concat(
+  symbols(91, 96).concat(
+  symbols(123, 126)
+  ))
+)
+
+// iterates over object and maps to array
+function lowerCharacters(low, high) {
+  for(i = low; i <= high; i++){
+    lowerArray.push(String.fromCharCode(i))
+  }
+  return lowerArray;
+}
+
+function upperCharacters(low, high) {
+  for(i = low; i <= high; i++){
+    upperArray.push(String.fromCharCode(i))
+  }
+  return upperArray;
+}
+
+function numbers(low, high) {
+  for(i = low; i <= high; i++){
+    numberArray.push(String.fromCharCode(i))
+  }
+  return numberArray;
+}
+
+function symbols(low, high) {
+  for(i = low; i <= high; i++){
+    symbolArray.push(String.fromCharCode(i))
+  }
+  return symbolArray;
+}
+
+// display array of characters lower upper number symbol
+console.log(lower);
+console.log(upper);
+console.log(number);
+console.log(symbol);
+// console.log(lowerChar);
+
+// Makes array for each character type
+// uppercase array
+// const uppercaseChar = arrayCharacters(65, 90);
+// lowercase array
+// number array
+// symbols array
+
+// puts specified characters into an array
+// function arrayCharacters(low, high) {
+  // let array = []
+//   for (let i = low; i <= high; i++){
+//     String.fromCharCode(i);
+//     array.push(i);
+//   }
+//   return array;
+// }
+
+// console.log(uppercaseChar);
 // checks values of passwordPreferences modal
 // function checked() {
   // password criteria
